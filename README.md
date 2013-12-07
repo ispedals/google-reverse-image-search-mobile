3c6e6047167e92428c594106cd4d27daa6c5a33d
ispedals <supronitbat@xynl.at>
2013-12-07 13:00:00 -0500

    Cleanup
    * Change indent to two spaces
    * Fix selector to only work on http/https images
    * Remove done Todo
    * Fix description
    * Fix max version
    * Update build

--- README.md
+++ README.md
@@ -3,13 +3,9 @@
 Addon for Firefox for Android that allows one to search by an image on Google
 
 Long-tap an image and select ''Search Image on Google'' from the context menu.
-If ''Search Image on Google'' does not appear, the image cannot be searched. Some images are unsearchable even if the option does appear in the context menu.
+If ''Search Image on Google'' does not appear, the image cannot be searched.
 
 Requires Firefox for Android 18.0+
-
-##Todo
-* Change selector context so that only http/https located images can be searched. Currently, the option appears in the context menu for data uri image
-even though they are not searchable.
 
 ##License
 Google Reverse Image Search is licensed under the MIT License, Copyright (c) 2013 ispedals
--- bootstrap.js
+++ bootstrap.js
@@ -2,68 +2,77 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 
 function isPrivateTab(tab) {
-	return tab.browser.docShell.QueryInterface(Ci.nsILoadContext).usePrivateBrowsing;
+  return tab.browser.docShell.QueryInterface(Ci.nsILoadContext).usePrivateBrowsing;
 }
 
 let searcherId = null;
 
 function loadIntoWindow(window) {
-	if (!window)
-		return;
+  if (!window)
+    return;
 
-	searcherId = window.NativeWindow.contextmenus.add('Search Image on Google', window.NativeWindow.contextmenus.imageLocationCopyableContext, function (target) {
-		let imgUrl = target.src;
-		window.BrowserApp.addTab('https://encrypted.google.com/searchbyimage?image_url=' + encodeURIComponent(imgUrl), {
-			isPrivate: isPrivateTab(window.BrowserApp.selectedTab)
-		});
-	});
+  searcherId = window.NativeWindow.contextmenus.add(
+    'Search Image on Google',
+    {
+      matches: function(element) {
+        // imageLocationCopyableContext also matches data urls, which google image search does not support
+        return window.NativeWindow.contextmenus.imageLocationCopyableContext.matches(element) && element.currentURI.scheme.startsWith('http');
+      }
+    },
+    function (target) {
+      let imgUrl = target.src;
+      window.BrowserApp.addTab('https://encrypted.google.com/searchbyimage?image_url=' + encodeURIComponent(imgUrl), {
+        isPrivate: isPrivateTab(window.BrowserApp.selectedTab)
+      });
+    }
+  );
 }
 
 function unloadFromWindow(window) {
-	if (!window)
-		return;
-	window.NativeWindow.contextmenus.remove(searcherId);
+  if (!window)
+    return;
+  window.NativeWindow.contextmenus.remove(searcherId);
 }
 
 var windowListener = {
-	onOpenWindow: function (aWindow) {
-		// Wait for the window to finish loading
-		let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
-		domWindow.addEventListener("UIReady", function () {
-			domWindow.removeEventListener("UIReady", arguments.callee, false);
-			loadIntoWindow(domWindow);
-		}, false);
-	},
-	onCloseWindow: function (aWindow) {},
-	onWindowTitleChange: function (aWindow, aTitle) {}
+  onOpenWindow: function (aWindow) {
+    // Wait for the window to finish loading
+    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+    domWindow.addEventListener("UIReady", function () {
+      domWindow.removeEventListener("UIReady", arguments.callee, false);
+      loadIntoWindow(domWindow);
+    }, false);
+  },
+  onCloseWindow: function (aWindow) {},
+  onWindowTitleChange: function (aWindow, aTitle) {}
 };
 
 function startup(aData, aReason) {
-	let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
-	// Load into any existing windows
-	let windows = wm.getEnumerator("navigator:browser");
-	while (windows.hasMoreElements()) {
-		let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-		loadIntoWindow(domWindow);
-	}
-	// Load into any new windows
-	wm.addListener(windowListener);
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+  // Load into any existing windows
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    loadIntoWindow(domWindow);
+  }
+  // Load into any new windows
+  wm.addListener(windowListener);
 }
 
 function shutdown(aData, aReason) {
-	// When the application is shutting down we normally don't have to clean
-	// up any UI changes made
-	if (aReason == APP_SHUTDOWN)
-		return;
-	let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
-	// Stop listening for new windows
-	wm.removeListener(windowListener);
-	// Unload from any existing windows
-	let windows = wm.getEnumerator("navigator:browser");
-	while (windows.hasMoreElements()) {
-		let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-		unloadFromWindow(domWindow);
-	}
+  // When the application is shutting down we normally don't have to clean
+  // up any UI changes made
+  if (aReason == APP_SHUTDOWN)
+    return;
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+  // Stop listening for new windows
+  wm.removeListener(windowListener);
+  // Unload from any existing windows
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    unloadFromWindow(domWindow);
+  }
 }
 
 function install(aData, aReason) {}
